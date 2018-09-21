ddc9dcc489b4855e09b2a3415a10e6bfac51ba84
Lucas Berta da Silva <lucasbertadasilva@Lucass-MacBook-Pro.local>
2018-09-20 21:39:19 -0300

    app server correction

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Http = require('./src/core/http');
 require('./src/routes/routes');
 
-const hostname = 'https://validity-control-casa-brasil.herokuapp.com/';
-const port = 8080;
+// const hostname = 'https://validity-control-casa-brasil.herokuapp.com/';
+const port = process.env.PORT || 3000;;
 
 Http.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/api/`);
+  console.log(`Server running at port ${port}`);
 });
