ee8cc1e5a0165c2368450925ed32e5fd9855375d
Lucas Berta da Silva <lucasbertadasilva@Lucass-MacBook-Pro.local>
2018-09-20 22:04:01 -0300

    app server correction

--- src/core/http.js
+++ src/core/http.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const Http = express();
 
 Http.use(bodyParser.json());
-Http.use(express.static('../../dist/'));
 Http.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
