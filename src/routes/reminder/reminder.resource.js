e7d275046753e054e1f18de77bc4ec1ade28f482
Lucas Berta <lucasberta59@gmail.com>
2018-10-15 15:09:52 -0300

    Update reminder.resource.js

--- src/routes/reminder/reminder.resource.js
+++ src/routes/reminder/reminder.resource.js
@@ -2,6 +2,10 @@ const Http = require('../../core/http');
 const Endpoints = require('../../core/endpoints');
 const ReminderDB = require('./reminder.db');
 
+Http.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 Http.get(Endpoints.REMINDER, (req, res) => {
   if (req.query.nearestDue === 'true') {
     ReminderDB.findNearestDue(req, (err, reminders) => {
